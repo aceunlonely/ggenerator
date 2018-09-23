66fe05a26186576ced7f9cb5ae1dd205474170da
apporoad <apporoadina@163.com>
2018-09-23 13:14:03 +0800

    anno

--- run.js
+++ run.js
@@ -4,18 +4,20 @@ var me = require('ggenerator-engine')
 
 var program = require('commander');
 
-
+var config =require('peeriocjs').invoke("config").sync.config();
 //  node run.js -d F:\test\ggenerator/d.zip -p test -t F:\test\ggenerator\tgt
 //get params
 program.version('1.0.0')
-    .option('-d --data [value]', '动态数据包路径,或者模板路径', null)
-    .option('-p --templateName [value]', '模板名')
-    .option('-t --targetPath [value]', '目标地址')
+    .usage('[options] <file ...>')
+    .option('-d --data [value]', '\r\n\t可空\r\n\t动态数据包路径,支持ddata文件夹,ddataZip,DataObject文件夹\r\n\t支持单个数据源JSON或者js，并支持相对路径\r\n\t空值时,说明调用的combo模板的TDData', null)
+    .option('-p --templateName [value]', '模板名，或者模板文件夹路径、或者模板zip路径，并支持相对路径')
+    .option('-t --targetPath [value]', '目标地址,支持相对路径，空值时，将存到全局配置：' + config.workplaceDefaultPath )
     .option('-u --debug <debug>', 'debug use', /^(true|false)$/i, "false")
     .option('-o --operation <exe>', 'operation', 'exe')
-    .option('-w --workplace [value]', '工作目录')
+    .option('-w --workplace [value]', '工作目录，默认值是全局配置:' + config.workplaceDefaultPath)
     .parse(process.argv);
 
+    //process.cwd() todo
 
 switch (program.operation) {
     case 'exe':
