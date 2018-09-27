eaf0110fc086fe8205dca33d95051c362418b34a
apporoad <apporoadina@163.com>
2018-09-28 07:46:39 +0800

    supportRelativeDir

--- run.js
+++ run.js
@@ -10,6 +10,9 @@ var path= require('path')
 var program = require('commander');
 
 var config =require('peeriocjs').invoke("config").sync.config();
+var utils = require('./utils')
+
+var currentPath = process.cwd()
 //  node run.js -d F:\test\ggenerator/d.zip -p test -t F:\test\ggenerator\tgt
 //get params
 program.version('1.0.0')
@@ -20,14 +23,14 @@ program.version('1.0.0')
     + '\r\n\ttepmlateName ddata')
     .option('-d --data [value]', '\r\n\t可空\r\n\t动态数据包路径,支持ddata文件夹,ddataZip,DataObject文件夹\r\n\t支持单个数据源JSON或者js，并支持相对路径\r\n\t空值时,说明调用的combo模板的TDData', null)
     .option('-p --templateName [value]', '模板名，或者模板文件夹路径、或者模板zip路径，并支持相对路径')
-    .option('-t --targetPath [value]', '目标地址,支持相对路径，空值时，将存到全局配置：' + config.workplaceDefaultPath )
+    .option('-t --targetPath [value]', '目标地址,支持相对路径，空值时，为当前路径' + currentPath ,"./","./")
     .option('-u --debug <debug>', 'debug use', /^(true|false)$/i, "false")
     .option('-o --operation <exe>', 'operation', 'exe')
     .option('-w --workplace [value]', '工作目录,支持相对路径，默认值是全局配置:' + config.workplaceDefaultPath)
     .parse(process.argv);
 
     //process.cwd() todo
-    var currentPath = process.cwd()
+    
     var p = {
         dDataPath: program.data,
         templatePackage: program.templateName,
@@ -48,19 +51,19 @@ program.version('1.0.0')
     //相对目录支持
     if(p.templatePackage && !path.isAbsolute(p.templatePackage)){
         if(fs.existsSync(path.join(currentPath,p.templatePackage))){
-            p.templatePackage = path.join(currentPath,p.templatePackage)
+            p.templatePackage = utils.getRigthTgt(currentPath,p.templatePackage)
         }
     }
     if(p.dDataPath && !path.isAbsolute(p.dDataPath)){
         if(fs.existsSync(path.join(currentPath,p.dDataPath))){
-            p.dDataPath = path.join(currentPath,p.dDataPath)
+            p.dDataPath = utils.getRigthTgt(currentPath,p.dDataPath)
         }
     }
     if(p.targetPath && !path.isAbsolute(p.targetPath)){
-        p.targetPath = path.join(currentPath,p.targetPath)
+        p.targetPath = utils.getRigthTgt(currentPath,p.targetPath) // path.join(currentPath,p.targetPath)
     }
     if(p.workplace && !path.isAbsolute(p.workplace)){
-        p.workplace = path.join(currentPath,p.workplace)
+        p.workplace = utils.getRigthTgt(currentPath,p.workplace)
     }
 
 
---
+++ utils.js
@@ -0,0 +1,42 @@
+
+var path = require('path')
+var fs = require('fs')
+
+ var startWith=function(str,s){
+    if(s==null||s==""||str.length==0||s.length>str.length)
+     return false;
+    if(str.substr(0,s.length)==s)
+       return true;
+    else
+       return false;
+   }
+
+// 获取正确的目标地址
+var getRigthTgt= function(workspace,tgt){
+
+    if(startWith(tgt,'./'))
+    {
+        tgt = tgt.substr(2)
+    }
+    if(startWith(tgt,'../'))
+    {
+        //path.basename
+        var newWorkspace = path.dirname(workspace)
+        if(newWorkspace == '.') throw new  Error('fom:getTgt: dont have ../ :' + workspace + "|" + tgt)
+        
+        //recurse
+        tgt = getRigthTgt(newWorkspace,tgt.substr(3))
+    }
+    else
+    {
+        tgt = path.join(workspace,tgt)
+    }
+    if(!fs.existsSync(path.dirname(tgt)))
+    {
+        gu.mkdirSync(path.dirname(tgt));
+        //fs.mkdirSync(path.dirname(tgt));
+    }
+    return tgt
+ }
+
+ exports.getRigthTgt = getRigthTgt;
