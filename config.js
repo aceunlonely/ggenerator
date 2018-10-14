049381e9cc2454f52cf7e2e98d286ae00741e9d8
holyond <holyond@163.com>
2018-10-14 22:27:11 +0800

    addConfig

--- config.js
+++ config.js
@@ -1,11 +1,35 @@
+var storage = require('mini-dbx')
+var db = storage(__dirname + '/config.json')
+var uicli = require('uicli.js')
 
-var config = require('./config.json')
+var getConfig= function(){
+    var config = {
+        "workplaceDefaultPath":null, 
+        "templatePackagesDefaultPath":null,
+        "renderIgnoreFileTypes":"zip,rar,7z,tar,gz,iso,doc,docx,pdf,wps,odf,png,gif,jpg",
+        "verbose" :true
+    }
+    var clone = Object.assign({}, config)
+    db.select('config', '#==0',function(err,list){
+    	if(err){return;}
+    	if(list&& list.length>0){
+            config = list[0]
+        }
+        else
+        {
+            db.insert('config', clone,function(err,inserted){
 
-config.workplaceDefaultPath = config.workplaceDefaultPath || (__dirname + "/workplace")
-config.isTest = false
-config.renderIgnoreFileTypes =  config.renderIgnoreFileTypes||  "zip,rar,7z,tar,gz,iso,doc,docx,pdf,wps,odf,png,gif,jpg"
-config.templatePackagesDefaultPath = config.templatePackagesDefaultPath || __dirname + "/templatePackages"
-config.verbose = config.verbose || true
+            });
+        }
+    });
+
+    config.workplaceDefaultPath = config.workplaceDefaultPath || (__dirname + "/workplace")
+    config.isTest = false
+    config.renderIgnoreFileTypes =  config.renderIgnoreFileTypes||  "zip,rar,7z,tar,gz,iso,doc,docx,pdf,wps,odf,png,gif,jpg"
+    config.templatePackagesDefaultPath = config.templatePackagesDefaultPath || __dirname + "/templatePackages"
+    config.verbose = config.verbose || true
+    return config
+}
 
 // var config={
 //     workplaceDefaultPath : __dirname + "/workplace",
@@ -16,6 +40,19 @@ config.verbose = config.verbose || true
 
 //console.log('config')
 
-require('peeriocjs').reg("config",function(){return config;},null,true)
+require('peeriocjs').reg("config",getConfig,null,true)
+require('peeriocjs').reg("setConfig",function(){
+    var configLust = {
+        "workplaceDefaultPath":null, 
+        "templatePackagesDefaultPath":"???(string)["+process.cwd()+"]配置模板所在路径",
+        "renderIgnoreFileTypes":"???(string)[zip,rar,7z,tar,gz,iso,doc,docx,pdf,wps,odf,png,gif,jpg]渲染忽略后缀列表",
+        "verbose" :true
+    }
+    var rc = uicli.uiGetJson(configLust).then(data=>{
+        db.update('config', data, '',function(err,updated){
+            if(!err) console.log('config success:' +JSON.stringify(data));
+        });
+    })
+},null,true)
 
 //module.exports= config
